d3385b81254fe84d1dcc4b876889697fcd05c87b
Ryot814 <134674496+Ryot814@users.noreply.github.com>
2024-08-24 16:16:11 +0900

    adapt overlap drawing

--- macros/resultOverView/ResultOverview.cxx
+++ macros/resultOverView/ResultOverview.cxx
@@ -10,23 +10,21 @@
 #include <TH1.h>
 #include <TString.h>
 
-std::vector<std::string> filePathVec;
-
-void obtainFilePath(std::string dataName)
+void obtainFilePath(std::string dataName, std::vector<std::string> *filePathVec)
 {
-  std::string directory = "../../data";
+  std::string directory = "../../temp";
   std::regex file_pattern(dataName + "_\\d{3}\\.root$");
 
   for (const auto &entry : std::filesystem::directory_iterator(directory)) {
     if (std::regex_search(entry.path().filename().string(), file_pattern)) {
-      filePathVec.push_back(entry.path().string());
+      filePathVec->push_back(entry.path().string());
     }
   }
 
-  std::sort(filePathVec.begin(), filePathVec.end());
+  std::sort(filePathVec->begin(), filePathVec->end());
 }
 
-void readHistAndDraw(std::string filePath, std::string channel)
+void readHistAndDraw(std::string filePath, std::string channel, int lineColor, bool isOverlap)
 {
   TFile *file = TFile::Open(filePath.c_str());
   if (!file || file->IsZombie()) {
@@ -43,6 +41,7 @@ void readHistAndDraw(std::string filePath, std::string channel)
     return;
   }
 
+  /*
   // search for minimum filled bin and maximum filled bin
   int min_bin = 1;
   int max_bin = hist->GetNbinsX();
@@ -62,24 +61,55 @@ void readHistAndDraw(std::string filePath, std::string channel)
   }
 
   hist->GetXaxis()->SetRange(min_bin, max_bin);
+  */
 
+  // set x axis range [700, 1100]
+  hist->GetXaxis()->SetRangeUser(700., 1000.);
+
+  hist->SetLineColor(lineColor);
   hist->SetTitle(filePath.c_str());
-  hist->Draw();
+  if (isOverlap) {
+    hist->Draw("same");
+  } else {
+    hist->Draw();
+  }
 
   return;
 }
 
 int main(int argc, char *argv[]) {
 
-  if (argc != 3) {
+  if (!(argc == 3 || argc == 4)) {
     std::cerr << "Incorrect arguments." << std::endl;
     return 1;
   }
 
-  std::string dataName = argv[1];
-  std::string channel = argv[2];
+  std::vector<std::string> filePathVec;
+  std::vector<std::string> filePathVec2;
 
-  obtainFilePath(dataName);
+  std::string dataName;
+  std::string dataName2;
+  std::string channel;
+
+  if (argc == 3) {
+    dataName = argv[1];
+    channel = argv[2];
+  } else if (argc ==4) {
+    dataName = argv[1];
+    dataName2 = argv[2];
+    channel = argv[3];
+  }
+
+  obtainFilePath(dataName, &filePathVec);
+  if (argc ==4) obtainFilePath(dataName2, &filePathVec2);
+
+  // if the number of files to load two sets of data are different exit.
+  if (argc == 4) {
+    if (!(filePathVec.size() == filePathVec2.size())) {
+      std::cerr << "the number of files to load two sets of data are different!" << std::endl;
+      return 2;
+    }
+  }
 
   bool isCanvasOver7 = (filePathVec.size() >= 8);
 
@@ -92,8 +122,13 @@ int main(int argc, char *argv[]) {
       c1->Divide(4, 2);
     }
     std::string filePath = filePathVec.at(i);
+
     c1->cd(i%8 + 1);
-    readHistAndDraw(filePath, channel);
+    readHistAndDraw(filePath, channel, 632, false); // 632 is kRed
+    if (argc == 4) {
+      std::string filePath2 = filePathVec2.at(i);
+      readHistAndDraw(filePath2, channel, 600, true); // 600 is kBlue
+    }
 
     if (i == 7) c1->SaveAs(Form("%s.ADC_dis_overview.pdf(", dataName.c_str()));
     else if ((i + 1) %8 == 0) c1->SaveAs(Form("%s.ADC_dis_overview.pdf", dataName.c_str()));
